7bb79fb7133db3802ad38b7bc1ef10412322fece
AamirBughio <aamirhashim95@gmail.com>
2024-12-22 12:25:34 +0500

    Add files via upload

---
+++ 22.12.2024/app.js
@@ -0,0 +1,100 @@
+//Chapter No 6
+//Question No 1
+
+// let a = 10;
+// document.write("Result:" + "<br></br>")
+// document.write("The value of a is: " + a + "<br><br>");
+
+// document.write("The value of ++a is: " + (++a) + "<br>");
+// document.write("Now the value of a is: " + a + "<br><br>");
+
+// document.write("The value of a++ is: " + (a++) + "<br>");
+// document.write("Now the value of a is: " + a + "<br><br>");
+
+// document.write("The value of --a is: " + (--a) + "<br>");
+// document.write("Now the value of a is: " + a + "<br><br>");
+
+// document.write("The value of a-- is: " + (a--) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+//Question No 2
+
+// let a = 2, b = 1;
+// let result = --a - --b + ++b + b--;
+
+// document.write("Final values:<br>");
+// document.write("a = " + a + "<br>");
+// document.write("b = " + b + "<br>");
+// document.write("result = " + result + "<br>");
+
+//Question No 3
+
+// let userName = prompt("Please enter your name:");
+
+// if (userName) {
+//     alert("Hello, " + userName + "! Welcome!");
+// } 
+
+//Question No 4
+//Chatgpt
+//  let userInput = prompt("Enter a number to display its multiplication table (default is 5):");
+
+//  let number = parseInt(userInput);
+//  if (isNaN(number)) {
+//      number = 5;
+//  }
+
+//  let table = "Multiplication Table of " + number + ":\n";
+//  for (let i = 1; i <= 10; i++) {
+//      table += `${number} x ${i} = ${number * i}\n`; 
+//  }
+// alert(table);
+
+
+//USER INPUT
+
+//Question No 1
+
+// let city = prompt("Enter your city name:");
+
+// if (city === "Karachi" , "karachi") {
+//     alert("Welcome to the city of lights");
+// }
+
+//Question No 2
+
+// let gender = prompt("Enter your gender (male/female):");
+
+// if (gender === "male") {
+//     alert("Good Morning Sir");
+// } 
+// if (gender === "female") {
+//     alert("Good Morning Ma'am");
+// } 
+
+//Question No 4
+
+// let fuel = prompt("Enter remaining fuel in your car (in litres):");
+
+// if (fuel <0.25) {
+//     alert("Please refill the fuel in your car");
+// }
+
+//Question No 5
+
+//  var a = 4;
+//  if (++a === 5){
+//  alert("given condition for variable a is true");
+//  }
+
+//  var b = 82;
+//  if (b++ === 83){
+//  alert("given condition for variable b is true");
+//  }
+
+// var c = 12;
+// if (c++=== 13){
+// alert("condition 1 is true");
+// }
+
+//https://github.com/AamirBughio/Java-Script-Assignment-3.git
